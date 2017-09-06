1d816387702b724f1535224ba58f58e383f9670e
yangangchen <yangangchen.0@gmail.com>
2017-09-05 20:31:11 -0400

    Final version!

--- gitupload.sh
+++ gitupload.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-git init
-git remote add origin https://github.com/yangangchen/Data_Science_Specialization_Projects.git
-git branch gh-pages
+#git init
+#git remote add origin https://github.com/yangangchen/Data_Science_Specialization_Projects.git
+#git branch gh-pages
 git remote show origin
 git add .
 git commit -m 'Final version!'
